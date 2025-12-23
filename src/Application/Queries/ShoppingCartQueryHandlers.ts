e2aa4b72adbb2aa37344a7823b8ab347a1e44b25
Antônio Falcão Jr <arfj@edu.univali.br>
2025-12-23 12:07:19 -0300

    Refactor ShoppingCartQueryHandlers to import IProjectionGateway interface

--- src/Application/Queries/ShoppingCartQueryHandlers.ts
+++ src/Application/Queries/ShoppingCartQueryHandlers.ts
@@ -1,8 +1,4 @@
-export interface IProjectionGateway {
-  findById<T>(collectionName: string, id: string): Promise<T | null>;
-  findAll<T>(collectionName: string): Promise<T[]>;
-  findByCustomerId<T>(collectionName: string, customerId: string): Promise<T[]>;
-}
+import { IProjectionGateway } from '../Gateways/IProjectionGateway';
 
 export interface ShoppingCartReadModel {
   cartId: string;
